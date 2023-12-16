81bcb9d2eda0c0bef42000d91d0afbdf61153c9e
Lovedeep <gurugobind.sh@gmail.com>
2023-12-16 20:21:24 +0530

    add Models

---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Lovedeep on 16/12/23.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+       let name: String
+       let url: String
+}
